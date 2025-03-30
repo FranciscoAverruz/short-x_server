6a2763973e246cbce5ca4525cab0eae3ca9e0470
FAverruz <146257059+FAverruz@users.noreply.github.com>
2025-03-30 20:28:20 +0200

    format code with Prettier

--- utils/createValidatedUser.js
+++ utils/createValidatedUser.js
@@ -1,9 +1,24 @@
-const User = require("../features/users/User.model"); 
+const User = require("../features/users/User.model");
 
-const createValidatedUser = async ({ username, email, password, confirmPassword, urls, isAdmin, plan }) => {
-
-  if (!password || password.length < 5 || !/[A-Z]/.test(password) || !/\d/.test(password) || !/[!@#$%^&*]/.test(password)) {
-    throw new Error("La contraseña debe tener al menos 5 caracteres, incluir una mayúscula, un número y un símbolo especial.");
+const createValidatedUser = async ({
+  username,
+  email,
+  password,
+  confirmPassword,
+  urls,
+  isAdmin,
+  plan,
+}) => {
+  if (
+    !password ||
+    password.length < 5 ||
+    !/[A-Z]/.test(password) ||
+    !/\d/.test(password) ||
+    !/[!@#$%^&*]/.test(password)
+  ) {
+    throw new Error(
+      "La contraseña debe tener al menos 5 caracteres, incluir una mayúscula, un número y un símbolo especial."
+    );
   }
 
   if (password !== confirmPassword) {
@@ -15,11 +30,14 @@ const createValidatedUser = async ({ username, email, password, confirmPassword,
     throw new Error("Este correo ya está registrado.");
   }
 
-  const newUser = await User.signup(username, email, isAdmin, urls, password, plan);
-
-  return newUser;
+  return {
+    username,
+    email,
+    isAdmin,
+    urls,
+    password,
+    plan,
+  };
 };
 
 module.exports = createValidatedUser;
-
-  
